d3d5cf6d4a32c49e3b5acbdcdfb1f41cfaf0b233
Yoshiaki Onoda <y-onoda@nikki-grp.co.jp>
2022-09-27 17:50:00 +0900

    テストデータを準備する。

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+var sources = [
+    {name: "太田",   reserve: 2, hold: 2,  want: 6,  week: 36},
+    {name: "熊谷",   reserve: 2, hold: 1,  want: 7,  week: 35},
+    {name: "鶴ヶ島", reserve: 2, hold: 2,  want: 5,  week: 35},
+    {name: "岡崎",   reserve: 2, hold: 6,  want: 0,  week: 36},
+    {name: "浜松",   reserve: 2, hold: 12, want: -2, week: 36},
+    {name: "ｽﾏｰｸ",   reserve: 2, hold: 2,  want: 7,  week: 35},
+    {name: "本社",   reserve: 0, hold: 7,  want: 0,  week: 99},
+];
+
+console.log(sources);
