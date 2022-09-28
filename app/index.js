d2c4c93a1f90e75bf16f4695ef25b1a8d315ccf7
Yoshiaki Onoda <y-onoda@nikki-grp.co.jp>
2022-09-28 10:06:18 +0900

    各拠点で保持すべき最小数を引いた数を移動可能な数とする。

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-var sources = [
+var sources = init([
     {name: "太田",   reserve: 2, hold: 2,  want: 6,  week: 36},
     {name: "熊谷",   reserve: 2, hold: 1,  want: 7,  week: 35},
     {name: "鶴ヶ島", reserve: 2, hold: 2,  want: 5,  week: 35},
@@ -6,7 +6,7 @@ var sources = [
     {name: "浜松",   reserve: 2, hold: 12, want: -2, week: 36},
     {name: "ｽﾏｰｸ",   reserve: 2, hold: 2,  want: 7,  week: 35},
     {name: "本社",   reserve: 0, hold: 7,  want: 0,  week: 99},
-];
+]);
 
 //console.log(sources);
 // 週番号
@@ -14,4 +14,21 @@ var xs = sources.map(x => x.week)
 .reduce((ax, x) => {ax[x] = x;return ax;}, [])
 .filter(x => x > 0)
 .sort();
-console.log(xs);
+//console.log(xs);
+
+function init(sources) {
+  // 最低保持台数をマイナスする。
+  return sources.map(x => {
+    // 余っている数
+    let movable = x.hold - x.reserve
+    return {
+      name: x.name,
+      reserve: x.reserve,
+      hold: movable,
+      want: (movable<0) ? (x.want - movable) : x.want,
+      week: x.week,
+      in: {},
+      out: {},
+    }
+  })
+}
